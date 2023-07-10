e538bfce2d946ae3426a40c2a9f45ef9196df4f4
Elizabeth Gugliotti <elizabeth.gugliotti@noaa.gov>
2023-07-10 19:25:45 +0000

    update r

--- .devcontainer/build-ss3.sh
+++ .devcontainer/build-ss3.sh
@@ -1,18 +1,18 @@
 sudo apt-get update
 wget https://github.com/admb-project/admb/releases/download/admb-13.1/admb-13.1-linux.zip
-sudo unzip admb-13.1-linux.zip && mv admb-13.1 admb
+sudo unzip admb-13.1-linux.zip -d /usr/local/bin
 sudo rm admb-13.1-linux.zip
-sudo chmod 777 admb
-sudo cp -Rvf admb /usr/local/admb
-export PATH=$PATH:/usr/local/admb/bin
+sudo chmod 755 /usr/local/bin/admb-13.1/bin/admb
+export PATH=$PATH://usr/local/bin/admb-13.1/bin
 
-cd /workflows/ss3-build-devcontainer
+cd /workspaces/ss3-build-devcontainer
 wget https://github.com/nmfs-stock-synthesis/stock-synthesis/archive/main.zip
 sudo unzip -o main.zip -d /usr/local/bin
 sudo chmod 777 /usr/local/bin/stock-synthesis-main
 cd /usr/local/bin/stock-synthesis-main
 sudo make
 mv /usr/local/bin/stock-synthesis-main/ss /workspaces/ss3-build-devcontainer/ss
+
 
 echo 'options(repos = c(CRAN = \"https://cloud.r-project.org\"))'
 sudo sh -c 'cat - >>\"${R_HOME}/etc/Rprofile.site\"'
