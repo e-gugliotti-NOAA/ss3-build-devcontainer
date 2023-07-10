ae83e4e8e662698f4a3e5e56c2b55b1a1528ae36
Elizabeth Gugliotti <elizabeth.gugliotti@noaa.gov>
2023-07-10 18:43:29 +0000

    update cd

--- .devcontainer/build-ss3.sh
+++ .devcontainer/build-ss3.sh
@@ -6,13 +6,13 @@ sudo chmod 777 admb
 sudo cp -Rvf admb /usr/local/admb
 export PATH=$PATH:/usr/local/admb/bin
 
-cd /workflows/github-actions-test
+cd /workflows/ss3-build-devcontainer
 wget https://github.com/nmfs-stock-synthesis/stock-synthesis/archive/main.zip
-sudo unzip main.zip -d /usr/local/bin
+sudo unzip -o main.zip -d /usr/local/bin
 sudo chmod 777 /usr/local/bin/stock-synthesis-main
 cd /usr/local/bin/stock-synthesis-main
 sudo make
-mv /usr/local/bin/stock-synthesis-main/ss /workspaces/github-actions-test/ss
+mv /usr/local/bin/stock-synthesis-main/ss /workspaces/ss3-build-devcontainer/ss
 
 echo 'options(repos = c(CRAN = \"https://cloud.r-project.org\"))'
 sudo sh -c 'cat - >>\"${R_HOME}/etc/Rprofile.site\"'
