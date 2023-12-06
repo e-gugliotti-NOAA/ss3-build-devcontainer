0e9f339dca1eb72d6ca943d8a203d7b116b68a5c
Elizabeth Perl (née Gugliotti) <elizabeth.gugliotti@noaa.gov>
2023-12-06 16:47:10 -0500

    Update build-ss3.sh

--- .devcontainer/build-ss3.sh
+++ .devcontainer/build-ss3.sh
@@ -5,13 +5,13 @@ sudo rm admb-13.1-linux.zip
 sudo chmod 755 /usr/local/bin/admb-13.1/bin/admb
 export PATH=$PATH:/usr/local/bin/admb-13.1/bin
 cd /workspaces/ss3-build-devcontainer
-wget https://github.com/nmfs-stock-synthesis/stock-synthesis/archive/main.zip
+wget https://github.com/nmfs-ost/ss3-source-code/archive/main.zip
 sudo unzip -o main.zip -d /usr/local/bin
-sudo chmod 777 /usr/local/bin/stock-synthesis-main
-cd /usr/local/bin/stock-synthesis-main
+sudo chmod 777 /usr/local/bin/ss3-source-code-main
+cd /usr/local/bin/ss3-source-code-main
 mkdir SS330
 /bin/bash ./Make_SS_330_new.sh -b SS330 -p
-mv /usr/local/bin/stock-synthesis-main/SS330/ss /workspaces/ss3-build-devcontainer/ss
+mv /usr/local/bin/ss3-source-code-main/SS330/ss /workspaces/ss3-build-devcontainer/ss
 
 
 echo 'options(repos = c(CRAN = \"https://cloud.r-project.org\"))'
