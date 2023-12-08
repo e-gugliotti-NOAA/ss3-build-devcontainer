d0fb9da377c02abefcd45431cfed874f67c13fde
Elizabeth Perl (née Gugliotti) <elizabeth.gugliotti@noaa.gov>
2023-12-08 14:42:00 +0000

    update

--- .devcontainer/build-ss3.sh
+++ .devcontainer/build-ss3.sh
@@ -3,17 +3,18 @@ wget https://github.com/admb-project/admb/releases/download/admb-13.1/admb-13.1-
 sudo unzip admb-13.1-linux.zip -d /usr/local/bin
 sudo rm admb-13.1-linux.zip
 sudo chmod 755 /usr/local/bin/admb-13.1/bin/admb
-# sudo export PATH=$PATH:/usr/local/bin/admb-13.1/bin >> $HOME/.bashrc
-echo "export PATH=/usr/local/bin/admb-13.1/bin:$PATH" >> $HOME/.bashrc
+export PATH=$PATH:/usr/local/bin/admb-13.1/bin
+echo "export PATH=/usr/local/bin/admb-13.1/bin:$PATH" >> ~/.bashrc
 cd /workspaces/ss3-build-devcontainer
 wget https://github.com/nmfs-ost/ss3-source-code/archive/main.zip
 sudo unzip -o main.zip -d /usr/local/bin
+sudo rm main.zip
 sudo chmod 777 /usr/local/bin/ss3-source-code-main
 cd /usr/local/bin/ss3-source-code-main
 mkdir SS330
 /bin/bash ./Make_SS_330_new.sh -b SS330 -p
 mv /usr/local/bin/ss3-source-code-main/SS330/ss /workspaces/ss3-build-devcontainer/ss
-sudo rm main.zip
+
 
 
 echo 'options(repos = c(CRAN = \"https://cloud.r-project.org\"))'
