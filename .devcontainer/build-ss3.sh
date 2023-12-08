7cdc0ac6470abe5dcc1b2af264baed289246edb3
Elizabeth Perl (née Gugliotti) <elizabeth.gugliotti@noaa.gov>
2023-12-08 14:28:27 +0000

    try another export

--- .devcontainer/build-ss3.sh
+++ .devcontainer/build-ss3.sh
@@ -3,7 +3,8 @@ wget https://github.com/admb-project/admb/releases/download/admb-13.1/admb-13.1-
 sudo unzip admb-13.1-linux.zip -d /usr/local/bin
 sudo rm admb-13.1-linux.zip
 sudo chmod 755 /usr/local/bin/admb-13.1/bin/admb
-export PATH=$PATH:/usr/local/bin/admb-13.1/bin
+# sudo export PATH=$PATH:/usr/local/bin/admb-13.1/bin >> $HOME/.bashrc
+echo "export PATH=/usr/local/bin/admb-13.1/bin:$PATH" >> $HOME/.bashrc
 cd /workspaces/ss3-build-devcontainer
 wget https://github.com/nmfs-ost/ss3-source-code/archive/main.zip
 sudo unzip -o main.zip -d /usr/local/bin
@@ -12,6 +13,7 @@ cd /usr/local/bin/ss3-source-code-main
 mkdir SS330
 /bin/bash ./Make_SS_330_new.sh -b SS330 -p
 mv /usr/local/bin/ss3-source-code-main/SS330/ss /workspaces/ss3-build-devcontainer/ss
+sudo rm main.zip
 
 
 echo 'options(repos = c(CRAN = \"https://cloud.r-project.org\"))'
